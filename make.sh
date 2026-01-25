54d1af5ed9707303a96dd75e407983ff6b02ae08
OgliariNatan <natanogliari@gmail.com>
2026-01-25 08:39:19 -0300

    [docs] update: ajustado referencias e opcao de minuta no fundo

--- make.sh
+++ make.sh
@@ -1,8 +1,13 @@
 #Script para compilação de documentos LaTeX com BibTeX
 # Autor: OgliariNatan
-# Atualizado em: 20250923
-pdflatex main.tex && bibtex main.aux && pdflatex main.tex && pdflatex main.tex
+# Atualizado em: 20260125
+rm -f main.aux main.bbl main.blg main.log main.out main.toc main.pdf main.fdb_latexmk
+
+#pdflatex -interaction=nonstopmode main.tex && \
+#bibtex main && \
+#pdflatex -interaction=nonstopmode main.tex && \
+#pdflatex -interaction=nonstopmode main.tex
 
 
 # Roda com erros
-#pdflatex main.tex ; bibtex main.aux ; pdflatex main.tex ; pdflatex main.tex
+pdflatex main.tex ; bibtex main; pdflatex main.tex ; pdflatex main.tex
